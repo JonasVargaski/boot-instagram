0846632582b2787cee4e4747b3ae636fa8681841
Jonas <jvargaski@exactsales.com.br>
2020-04-02 09:12:24 -0300

    add readme

---
+++ README.md
@@ -0,0 +1,43 @@
+## Instagram-BOT
+
+Webscraping para navegar pelo instagram, possibilitando seguir pessoas, páginas, hashtags, curtir conteudo de forma dinâmica, gerando intervalos de tempo aleatórios para evitar bloqueio de conta.
+
+Salva todos os links de curtidas, seguidores, etc, para não curtir novamente mais tarde.
+
+Ideal para alavancar páginas de conteudo, ganhar novos seguidores etc..
+
+### Tecnologias utilizadas
+
+- JavaScript
+- NodeJS
+- Puppeteer
+- Date fns
+
+### Pré-requisitos
+
+- [Node.js](https://nodejs.org "Node.js") >= 8.10 instalado
+
+### Como executar
+
+```sh
+1 > altere o arquivo .env.example para .env
+2 > insira os dados de login da sua conta no arquivo .env
+```
+
+Execute a aplicação em ambiente local, abra o diretório do projeto no terminal e execute os seguintes comandos:
+
+##### Instalar dependências do projeto:
+
+```sh
+yarn install
+ou
+npm install
+```
+
+##### Executar projeto em modo de desenvolvimento:
+
+```sh
+yarn start
+ou
+npm start
+```
--- src/actions/serachAndLike.js
+++ src/actions/serachAndLike.js
@@ -1,22 +1,22 @@
-const formatDate = require('date-fns/format')
-const store = require('../utils/store')
+const formatDate = require("date-fns/format");
+const store = require("../utils/store");
 
-const randomTimeA = require('../utils/randomTime')(2000, 7000, 500);
-const randomTimeB = require('../utils/randomTime')(1000, 3000, 200);
+const randomTimeA = require("../utils/randomTime")(2000, 7000, 500);
+const randomTimeB = require("../utils/randomTime")(1000, 3000, 200);
 
 module.exports = async function actionLikes(page, search) {
-  await page.waitForSelector('input[placeholder="Pesquisar"]')
+  await page.waitForSelector('input[placeholder="Pesquisar"]');
   await page.type('input[placeholder="Pesquisar"]', search.tag);
   await page.waitFor(2000);
-  await page.waitForSelector('a[href*="/explore/tags/"]')
+  await page.waitForSelector('a[href*="/explore/tags/"]');
   await page.click('a[href*="/explore/tags/"]');
-  await page.waitForSelector('a[href*="/p/"]')
+  await page.waitForSelector('a[href*="/p/"]');
 
   const hrefs = await page.$$('a[href*="/p/"');
 
   let like_count = 0;
-  const liked_hashtag = store.get('hashtags', {});
-  const following = store.get('following', {});
+  const liked_hashtag = store.get("hashtags", {});
+  const following = store.get("following", {});
 
   try {
     for await (let href of hrefs) {
@@ -28,17 +28,19 @@ module.exports = async function actionLikes(page, search) {
         const url = page.url();
         if (!following[url]) {
           await page.click('div[class*="f7QXd  HfISj"]>button[class*="sqdOP"]');
-          following[url] = formatDate(new Date(), 'dd/MM/yyyy HH:mm:ss');
+          following[url] = formatDate(new Date(), "dd/MM/yyyy HH:mm:ss");
         }
       }
 
-      const value = await (await href.getProperty('href')).jsonValue();
+      const value = await (await href.getProperty("href")).jsonValue();
 
       if (!liked_hashtag[value]) {
-        liked_hashtag[value] = formatDate(new Date(), 'dd/MM/yyyy HH:mm:ss');
+        liked_hashtag[value] = formatDate(new Date(), "dd/MM/yyyy HH:mm:ss");
         await href.click();
         await page.waitFor(randomTimeA());
-        await page.waitForSelector('span[class*="fr66n"]>button[class*="wpO6b"]');
+        await page.waitForSelector(
+          'span[class*="fr66n"]>button[class*="wpO6b"]'
+        );
         await page.click('span[class*="fr66n"]>button[class*="wpO6b"]');
         await page.waitFor(randomTimeB());
         like_count++;
@@ -46,10 +48,9 @@ module.exports = async function actionLikes(page, search) {
       }
     }
   } catch ({ message }) {
-    console.log('Erro ao curtir: ', message);
+    console.log("Erro ao curtir: ", message);
   } finally {
-    store.set('hashtags', liked_hashtag);
-    if (search.follow) store.set('following', following);
+    store.set("hashtags", liked_hashtag);
+    if (search.follow) store.set("following", following);
   }
-}
-
+};
